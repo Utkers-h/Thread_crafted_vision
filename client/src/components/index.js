15f98c8e35b58e3d7e4a2aaf8f4fa8c638e13e07
Utkersh uppal <utkersh2003uppal@gmail.com>
2023-08-14 00:49:53 +0530

    Deployment fix

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    Tab,
+    FilePicker
+};
